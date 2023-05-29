a686f38150f2d558a89bca036a6217b6181e5171
Lyuben Kikov <exceptionhelper@gmail.com>
2023-05-29 20:25:00 +0300

    Included ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselBasicComponent } from './carousel-basic/carousel-basic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselBasicComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    NgbCarouselModule,
     AppRoutingModule
   ],
   providers: [],
