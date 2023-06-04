00e04ea119b7a5c3770e45b7fd3f850cba23e3f7
Lyuben Kikov <exceptionhelper@gmail.com>
2023-06-04 15:41:42 +0300

    Updated path to images

--- src/app/tavern/tavern.component.ts
+++ src/app/tavern/tavern.component.ts
@@ -10,25 +10,25 @@ export class TavernComponent implements OnInit {
     {
       Title: "Две легла",
       Description: "Самостоятелна баня и тоалетна...",
-      Image: "./assets/images/tavern/06l.jpg",
+      Image: "../../assets/images/tavern/06l.jpg",
       Price: 29.95
     }, 
     {
       Title: "King Size Bed",
       Description: "Всичко включено.....",
-      Image: "./assets/images/tavern/images (1).jpeg",
+      Image: "../../assets/images/tavern/images (1).jpeg",
       Price: 39.22
     }, 
     {
       Title: "Ергенска спалня",
       Description: "Джакузи и тераса",
-      Image: "./assets/images/tavern/images.jpeg",
+      Image: "../../assets/images/tavern/images.jpeg",
       Price: 41.15
     },
     {
       Title: "Семейна стая за 4 човека",
       Description: "Самостоятелна кухня и паркинг",
-      Image: "./assets/images/tavern/367661620920142ecb14a1d9d7137ebda811.jpg",
+      Image: "../../assets/images/tavern/367661620920142ecb14a1d9d7137ebda811.jpg",
       Price: 41.15
     }
   ];
