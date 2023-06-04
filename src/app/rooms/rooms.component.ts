9eb570fd8a502a3f1646dafcb538077aff843ef5
Lyuben Kikov <exceptionhelper@gmail.com>
2023-06-04 15:16:49 +0300

    Fixed issue with path to images

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -11,25 +11,25 @@ export class RoomsComponent implements OnInit {
     {
       Title: "Две легла",
       Description: "Самостоятелна баня и тоалетна...",
-      Image: "../../assets/images/Bedroom1-Beds-61134884.jpg",
+      Image: "./assets/images/Bedroom1-Beds-61134884.jpg",
       Price: 29.95
     }, 
     {
       Title: "King Size Bed",
       Description: "Всичко включено.....",
-      Image: "../../assets/images/corinthia-kartoum-executive-room-2726875311.jpg",
+      Image: "./assets/images/corinthia-kartoum-executive-room-2726875311.jpg",
       Price: 39.22
     }, 
     {
       Title: "Ергенска спалня",
       Description: "Джакузи и тераса",
-      Image: "../../assets/images/th-378557538.jpg",
+      Image: "./assets/images/th-378557538.jpg",
       Price: 41.15
     },
     {
       Title: "Семейна стая за 4 човека",
       Description: "Самостоятелна кухня и паркинг",
-      Image: "../../assets/images/th-2283780811.jpg",
+      Image: "./assets/images/th-2283780811.jpg",
       Price: 41.15
     }
   ];
